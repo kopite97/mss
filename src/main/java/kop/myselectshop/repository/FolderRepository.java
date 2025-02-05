33b61f186708fcaaa808ce8def277eb5d64af3d1
Joon <tjrwns3428@gmail.com>
2025-02-05 23:29:41 +0900

    상품폴더 설계

---
+++ src/main/java/kop/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package kop.myselectshop.repository;
+
+import kop.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder,Long> {
+}
---
+++ src/main/java/kop/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package kop.myselectshop.repository;
+
+import kop.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder,Long> {
+}
